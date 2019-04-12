ef0f0ac5f290ef42164aa502d0febf508e732ac9
Roman Rieunier <rieunierroman@hotmail.fr>
2019-04-12 15:04:35 +0200

    Updates

---
+++ precache-manifest.b9ed88da2634e6031571ec680debe274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b9de7c0019602a421c5",
+    "url": "/daltonismulator/static/css/main.548e8ff9.chunk.css"
+  },
+  {
+    "revision": "2b9de7c0019602a421c5",
+    "url": "/daltonismulator/static/js/main.4d032107.chunk.js"
+  },
+  {
+    "revision": "2d926e41cc0ccb9f03e9",
+    "url": "/daltonismulator/static/js/runtime~main.0646eebc.js"
+  },
+  {
+    "revision": "8a3320023804cf1a7aeb",
+    "url": "/daltonismulator/static/js/2.f179fd93.chunk.js"
+  },
+  {
+    "revision": "f2a6aa7097fcc2b040d8e9f962db3d73",
+    "url": "/daltonismulator/static/media/eye.f2a6aa70.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daltonismulator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a916cac6bda94ffb43d76cc844494c3c",
+    "url": "/daltonismulator/index.html"
+  }
+];
